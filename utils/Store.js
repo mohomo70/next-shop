7905820efa70b35ea8ddd931cd66fbda79ce015f
unknown <mhmohammadi991@gmail.com>
2022-11-30 12:05:10 +0330

    'create-payment-method'

--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext()
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [] },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
 }
 
 function reducer(state, action) {
@@ -40,6 +40,26 @@ function reducer(state, action) {
           paymentMethod: '',
         },
       }
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
+        },
+      }
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
+        },
+      }
+
     default:
       return state
   }
